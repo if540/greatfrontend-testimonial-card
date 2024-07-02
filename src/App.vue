abc83e4ba33fe1ad011109a1d4a388f91911eb08
Kerwin <if5400@gmail.com>
2024-07-02 22:20:07 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import testimonialCard from './components/testimonialCard.vue'
+</script>
+
+<template>
+  <testimonialCard />
+</template>
+
---
+++ src/components/testimonialCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="max-w-96 mx-auto mt-32 text-center text-blue-500 rounded-full border-blue-500 border-2 border-solid px-1 py-2">
+    greatfrontend-testimonial-card
+  </div>
+</template>
+
+<style lang="scss">
+div {
+  color: #333;
+}
+</style>
